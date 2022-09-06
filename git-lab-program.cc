89cfe6f9f41eb015c308fc7b67c51249f0234c0d
Will Krietemeyer <wk343621@ohio.edu>
2022-09-06 17:27:39 -0400

    added git practice program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Will Krietemeyer
+ *        Date: 9/6/2022
+ * Description: This program is to practice pushing code to github
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
